66b2779b20235e93e9cbb8b1c9e8a85e4e7c1acb
Krishnasaianumandla <krishnasaieee1997@gmail.com>
2021-09-29 12:55:56 +0530

    completed implementation of drawGrid function

--- battleship.py
+++ battleship.py
@@ -31,8 +31,8 @@ def makeModel(data):
     data["cellSize"] = data["boardSize"]/data["no_of_cols"]
     data["no_of_ships"] = 5
     data["comp_board"] = emptyGrid(data["no_of_rows"],data["no_of_cols"])
-    #data["user_board"] = emptyGrid(data["no_of_rows"],data["no_of_cols"])
-    data["user_board"]= test.testGrid()
+    data["user_board"] = emptyGrid(data["no_of_rows"],data["no_of_cols"])
+    #data["user_board"]= test.testGrid()
     data["comp_board"] = addShips(data["comp_board"],data["no_of_ships"])   
     return
 
@@ -43,8 +43,6 @@ Parameters: dict mapping strs to values ; Tkinter canvas ; Tkinter canvas
 Returns: None
 '''
 def makeView(data, userCanvas, compCanvas):
-    userCanvas = drawGrid(data,userCanvas,data["user_board"], True)
-    compCanvas = drawGrid(data,compCanvas,data["comp_board"], True)
     return
 
 
@@ -137,7 +135,7 @@ def drawGrid(data, canvas, grid, showShips):
             else:
                 canvas.create_rectangle(data["cellSize"]*col,data["cellSize"]*row,data["cellSize"]*(col+1),data["cellSize"]*(row+1),fill="blue")
             
-    return
+    return data
 
 ### WEEK 2 ###
 
@@ -308,6 +306,6 @@ def runSimulation(w, h):
 if __name__ == "__main__":
 
     ## Finally, run the simulation to test it manually ##
-    runSimulation(500, 500)
+    #runSimulation(500, 500)
     #test.testAddShips()
-    #test.testMakeModel()
+    test.testDrawGrid()
